5af69c6f3c4c48cb6efcc22218fb06f7921084ab
Chaitanya Annangi <chaitanya698@gmail.com>
2019-04-03 08:53:36 +0530

    new test changes done

---
+++ src/stackOperations.java
@@ -0,0 +1,59 @@
+
+public class stackOperations {
+
+	    static final int MAX = 1000; 
+	    int top; 
+	    int a[] = new int[MAX]; // Maximum size of Stack 
+	  
+	    boolean isEmpty() 
+	    { 
+	        return (top < 0); 
+	    } 
+	    stackOperations() 
+	    { 
+	        top = -1; 
+	    } 
+	  
+	    boolean push(int x) 
+	    { 
+	        if (top >= (MAX-1)) 
+	        { 
+	            System.out.println("Stack Overflow"); 
+	            return false; 
+	        } 
+	        else
+	        { 
+	            a[++top] = x; 
+	            System.out.println(x + " pushed into stack"); 
+	            return true; 
+	        } 
+	    } 
+	  
+	    int pop() 
+	    { 
+	        if (top < 0) 
+	        { 
+	            System.out.println("Stack Underflow"); 
+	            return 0; 
+	        } 
+	        else
+	        { 
+	            int x = a[top--]; 
+	            return x; 
+	        } 
+	    } 
+	} 
+	  
+	// Driver code 
+	class Main 
+	{ 
+	    public static void main(String args[]) 
+	    { 
+	    	stackOperations s = new stackOperations(); 
+	        s.push(10); 
+	        s.push(20); 
+	        s.push(30); 
+	        System.out.println(s.pop() + " Popped from stack"); 
+	    } 
+	} 
+
